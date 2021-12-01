f4f7cf73be5b4f55d19039327155f08907d00cab
DinoGurnari <dinogurnari@gmail.com>
2021-12-01 14:51:34 +0100

    h

---
+++ sourceEtu/passeTypeRat.ml
@@ -0,0 +1,12 @@
+(* Module de la passe de gestion des types *)
+module PasseTypeRat : Passe.Passe with type t1 = Ast.AstTds.programme and type t2 = Ast.AstType.programme =
+struct
+
+  open Tds
+  open Exceptions
+  open Ast
+  open AstType
+  open Type
+
+  type t1 = Ast.AstTds.programme
+  type t2 = Ast.AstType.programme
